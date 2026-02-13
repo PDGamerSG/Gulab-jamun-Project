1249e0495c0fd785b2fec080c4e776328c3f9a31
Ayush <ayushyadav102030tt@gmail.com>
2026-02-13 11:05:19 +0530

    test

--- Backend/data_integration.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import random
-
-shipment_df1 = pd.read_csv("clean_shipment.csv")
-shipment_df1.to_parquet("fact_shipment.parquet", index=False)
-print(" Clean fact_shipment.parquet created")
-
-check_df = pd.read_parquet("fact_shipment.parquet")
-print(check_df.columns)
